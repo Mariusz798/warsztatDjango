0d6cefee0488407e60494200d11113ec608c3e36
Mariusz798 <mariusz042475@gmail.com>
2021-04-16 11:50:32 +0100

    init_pierwszy

--- warsztatDjango/settings.py
+++ warsztatDjango/settings.py
@@ -19,6 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = local_settings.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -67,6 +69,10 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'warsztatDjango.wsgi.application'
+
+
+DATABASES = local_settings.DATABASES
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
