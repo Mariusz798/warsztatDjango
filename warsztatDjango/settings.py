46778307aa5cac1f07a0e8ac54e9ac993282eff9
Mariusz798 <mariusz042475@gmail.com>
2021-04-16 11:43:57 +0100

    init_pierwszy

--- warsztatDjango/settings.py
+++ warsztatDjango/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import local_settings
+from warsztatDjango import local_settings
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
